39a250763f721fd65f9bcac7a648521ed2fdecd5
Vov4ikM <mirgorodskijvolodina@gmail.com>
2026-03-16 22:45:37 +0300

    Update main_with_comm2.py

--- LR2/main_with_comm2.py
+++ LR2/main_with_comm2.py
@@ -1,45 +1,61 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def E(theta): #Нахождение напряжённости элек.поля, там ещё оно номрмированно, т.к. сокращая при этом параметры некоторые
+def E(theta): 
     num = np.cos(k * l * np.cos(theta)) - np.cos(k * l)
     den = np.sin(theta)
     return num/den
 
 def F(theta):
-    return abs(E(theta)) / abs(E(theta).max())
+    return np.abs(E(theta)) / np.max(np.abs(E(theta)))
 
 def Dmax(theta):
-    formula = (F(theta)**2 * np.sin(theta))
-    return 2 / np.trapezoid(formula, theta) #Трапецоида = интеграл по theta
+    mask = theta <= np.pi
+    formula = (F(theta[mask])**2 * np.sin(theta[mask]))
+    integral = np.trapezoid(formula, theta[mask])
+    
+    if integral <= 0:
+        return 0
+    return 4 / integral 
  
-def D(theta): #Формула нахождения КНД
-    return F(theta)**2 * Dmax(theta)
+def D(theta): 
+    dmax = Dmax(theta)
+    if dmax <= 0:
+        return np.zeros_like(theta)
+    return F(theta)**2 * dmax
 
 def creating_plot(d_times, d_dB, theta):
-
     fig, axs = plt.subplots(2, 2, figsize=(12,10), subplot_kw={'polar': False})
     fig.suptitle('D(θ)')
 
-    axs[0,0].plot(theta, d_times, color='blue')
+   
+    theta_deg = np.degrees(theta)
+    
+    axs[0,0].plot(theta_deg, d_times, color='blue')
     axs[0,0].set_title("КНД (разы, декарт)")
-    axs[0,0].set_xlabel("θ (рад)")
+    axs[0,0].set_xlabel("θ (град)")
     axs[0,0].set_ylabel("D(θ)")
     axs[0,0].grid(True)
+    axs[0,0].set_xlim(0, 360)
 
-    axs[0,1].plot(theta, d_dB, color='red')
+    axs[0,1].plot(theta_deg, d_dB, color='red')
     axs[0,1].set_title("КНД (дБ, декарт)")
-    axs[0,1].set_xlabel("θ (рад)")
+    axs[0,1].set_xlabel("θ (град)")
     axs[0,1].set_ylabel("D(θ) [дБ]")
     axs[0,1].grid(True)
+    axs[0,1].set_xlim(0, 360)
 
     axs[1,0] = plt.subplot(2,2,3, polar=True)
     axs[1,0].plot(theta, d_times, color='blue')
     axs[1,0].set_title("КНД (разы, поляр)")
+    axs[1,0].set_theta_zero_location('N')  
+    axs[1,0].set_theta_direction(-1) 
 
     axs[1,1] = plt.subplot(2,2,4, polar=True)
     axs[1,1].plot(theta, d_dB, color='red')
     axs[1,1].set_title("КНД (дБ, поляр)")
+    axs[1,1].set_theta_zero_location('N')
+    axs[1,1].set_theta_direction(-1)
 
     plt.tight_layout(rect=[0, 0, 1, 0.96])
     plt.savefig('var13.png')
@@ -52,15 +68,17 @@ def main():
     lmbd = 3 * 10 ** 8 / f
     l = 2.5 * lmbd / 2
     k = 2 * np.pi / lmbd
-    theta = np.linspace(1e-9, np.pi-(1e-9), 2000)
+    theta = np.linspace(1e-9, 2 * np.pi - (1e-9), 4000)
 
-    print(f'{Dmax(theta=theta):.3f} times\n{10 * np.log10(Dmax(theta=theta)):.3f} dB') # .3f - форматирование чисел с плавающей точкой, до 3х символов после запятой
+    dmax_val = Dmax(theta=theta)
+    print(f'{dmax_val:.3f} times\n{10 * np.log10(dmax_val):.3f} dB')
     
-    creating_plot(d_times=D(theta), d_dB=10*np.log10(D(theta) + 1e-9), theta=theta) # 10*np.log() - перевод из d_times в дециБеллы
     d_times = D(theta)
-    d_db = 10*np.log10(D(theta) + 1e-9)
+    d_db = 10 * np.log10(d_times + 1e-9)
+    
+    creating_plot(d_times=d_times, d_dB=d_db, theta=theta)
 
-    with open('python_results.txt', 'w', encoding='utf-8') as file: #Запись в файл данные
+    with open('python_results.txt', 'w', encoding='utf-8') as file:
         file.write('theta   d_times   d_db\n')
         for i in range(len(theta)):
             file.write(f'{theta[i]}   {d_times[i]}   {d_db[i]}\n')
